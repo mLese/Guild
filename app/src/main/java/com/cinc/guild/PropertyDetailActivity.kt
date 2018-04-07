1581d0f27ebffda513ed74cb50935b45d8a7e2a6
Michael Lese <michael.lese08@gmail.com>
2018-04-07 13:27:58 -0400

    added comments

--- app/src/main/java/com/cinc/guild/PropertyDetailActivity.kt
+++ app/src/main/java/com/cinc/guild/PropertyDetailActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_property_detail.*
@@ -21,6 +22,7 @@ import java.util.concurrent.TimeUnit
 
 class PropertyDetailActivity : AppCompatActivity() {
 
+    // API Service Setup
     val client = OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(30, TimeUnit.SECONDS)
@@ -34,8 +36,11 @@ class PropertyDetailActivity : AppCompatActivity() {
 
     val api = retrofit.create(API::class.java)
 
+    // Property
     lateinit var property: Property
+    var favorite = false
 
+    // Views
     lateinit var toolbar: Toolbar
     lateinit var fab: FloatingActionButton
     lateinit var price: TextView
@@ -44,19 +49,20 @@ class PropertyDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_property_detail)
         val activity = this
 
+        // Setup View
+        setContentView(R.layout.activity_property_detail)
         toolbar = findViewById(R.id.toolbar)
         fab = findViewById(R.id.fab)
         price = findViewById(R.id.price)
         sqft = findViewById(R.id.sqft)
         description = findViewById(R.id.description)
 
+        // Setup Toolbar
         setSupportActionBar(toolbar)
 
-        var favorite = false
-
+        // Setup FAB
         fab.setOnClickListener({ view ->
             run {
                 favorite = ! favorite
@@ -66,20 +72,27 @@ class PropertyDetailActivity : AppCompatActivity() {
             }
         })
 
+        // Fetch Property
         val call = api.getPropertyDetails("FMLSRESALL590241548396306")
         call.enqueue(object: Callback<Property> {
             override fun onResponse(call: Call<Property>, response: Response<Property>) {
                 if (response.isSuccessful) {
                     property = response.body() as Property
 
+                    // Set Toolbar Title
                     collapsing_toolbar_layout.isTitleEnabled = false
-                    toolbar.title = "${property.streetAddress} ${property.county}"
+                    toolbar.title = "${property.streetAddress} - ${property.county}"
 
+                    // Set Banner Image
                     Glide.with(activity).load("http://" + property.media[0].mediaUrl).into(property_image)
+
+                    // Set Summary Card Info
                     price.text = property.priceFriendly
                     beds.text = "${property.beds} Beds"
                     baths.text = "${property.baths} Baths"
                     sqft.text = "${property.sqFtFriendly} Sqft"
+
+                    // Set Description Info
                     description.text = property.remarks
 
                 } else {
@@ -88,7 +101,9 @@ class PropertyDetailActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<Property>, t: Throwable) {
+                // Handle Error From Network
                 Log.e("Property Detail", "Network Error: " + t.localizedMessage)
+                Toast.makeText(activity, "Error Fetchin Property", Toast.LENGTH_LONG).show()
             }
         })
     }
