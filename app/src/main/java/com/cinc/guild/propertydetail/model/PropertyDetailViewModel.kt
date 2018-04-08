e9bd680ddfb618aa81a7b9649c902978493e91ea
Michael Lese <michael.lese08@gmail.com>
2018-04-08 12:07:01 -0400

    removing unused vars from view model

--- app/src/main/java/com/cinc/guild/propertydetail/model/PropertyDetailViewModel.kt
+++ app/src/main/java/com/cinc/guild/propertydetail/model/PropertyDetailViewModel.kt
@@ -3,8 +3,6 @@ package com.cinc.guild.propertydetail.model
 import com.cinc.guild.propertydetail.model.api.Property
 
 class PropertyDetailViewModel(property: Property) {
-
-    val pdid = property.pdid
     val bannerImageUrl = "http://${property.media[0].mediaUrl}"
     val title = "${property.streetAddress} ${property.county}"
     val price = property.priceFriendly
@@ -12,5 +10,4 @@ class PropertyDetailViewModel(property: Property) {
     val baths = property.baths.toString()
     val squareFootage = "${property.sqFtFriendly} sqft"
     val description = property.remarks
-    var liked = property.reaction != 0
 }
